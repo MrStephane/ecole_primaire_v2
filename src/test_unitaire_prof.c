e873f33e843107ebbdc9c7ff1eb0ead5d17adf6a
Aignan <stephane.aignan@gmail.com>
2016-01-22 17:06:18 +0100

    fichier de test des fonctions prof
    Permet de tester les fonctions sur les profs.

---
+++ src/test_unitaire_prof.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "prof.h"
+
+
+int main(int argc, char* argv[])
+{
+	Prof_t* ptr_prof;
+	
+	
+	ptr_prof = CreationProf();
+	
+	SaisirProf(ptr_prof);
+	
+	RetourLigne(2);
+	
+	AfficherProf(*ptr_prof);
+	
+	return 0;
+}
