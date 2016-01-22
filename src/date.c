6f5c9c25a21c2714b9981dc9f8f7989a4ef2476e
Martincl65 <martin.clement65@gmail.com>
2016-01-22 10:44:09 +0100

    modif SaisirDate

--- src/date.c
+++ src/date.c
@@ -25,9 +25,11 @@ void AfficherDate(struct tm ptr_d)
     printf("%d/%d/%d", &ptr_d.tm_mday, &ptr_d.tm_mon, &ptr_d.tm_year);
 }
 
-void SaisirDate(Date *d)
+void SaisirDate(struct tm *ptr_d)
 {
 	printf("(jj.mm.aaaa) : ");
-	scanf("%d.%d.%d", &d->jour, &d->mois, &d->an);
+	scanf("%d.%d.%d", &ptr_d->tm_mday, &ptr_d->tm_mon, &ptr_d->tm_year);
 	ViderBuffer();
+	ptr_d->tm_mday = ptr_d->tm_mday-1;
+	ptr_d->tm_year = ptr_d->tm_year-1900;
 }
