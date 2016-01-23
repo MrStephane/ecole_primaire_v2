57b809101cc4e66f13518a4d8fbd8816171ec0cf
Aignan <stephane.aignan@gmail.com>
2016-01-23 18:18:13 +0100

    Ajout de la fonction SupprimerEleveDansEcole
    Permet de supprimer un élève si il existe.

--- src/ecole.c
+++ src/ecole.c
@@ -2,6 +2,40 @@
 #include <stlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <string.h>
 #include <time.h>
+
 #include "classe.h"
+
+
+
+
+void SupprimerEleveDansEcole(Ecole_t *ptr_ecole)
+{
+	Eleve_t *ptr_eleve;
+	Classe_t *ptr_classe;
+	
+	char nom[TAILLECHAINE];
+	char prenom[TAILLECHAINE];
+	
+	
+	
+	printf("\n\tNom prenom : ");
+	scanf("%s %s", nom, prenom);
+	ViderBuffer();
+
+	NormaliserNomPrenom(NULL, nom, prenom);
+	
+	ptr_eleve = RechercherEleveDansEcole(ptr_ecole);
+	
+	if (ptr_eleve == NULL)
+	{
+		printf("Cet eleve n'exite pas.\n");
+		return;
+	}
+	
+	ptr_classe = RechercherClasseEleve(ptr_ecole->premiereClasse, ptr_eleve);
+	if (ptr_classe == NULL)
+		printf("Erreur dans la recherche de la classe");
+	else
+		SupprimerEleveDansClasse(ptr_classe, ptr_eleve);
+}
