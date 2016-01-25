1745ef08356c0c45b73b73362a4b1eb9ea24989f
Aignan <stephane.aignan@gmail.com>
2016-01-25 22:16:16 +0100

    Ajout de la description des fonctions de l'école.

--- src/ecole.h
+++ src/ecole.h
@@ -26,36 +26,36 @@ typedef struct Ecole
  *************/
 
 
-/* Fonction :
- * ----------
- * Entrée :
- * Sortie :
- *  Usage :
- * ----------
- * *Description*
+/* Fonction : AjouterEleveDansEcole
+ * --------------------------------
+ * Entrée : ptr_ecole - Pointeur sur l'école dans laquelle on veut ajouter l'élève.
+ * Sortie : Aucune.
+ *  Usage : AjouterEleveDansEcole(ptr_ecole);
+ * --------------------------------
+ * Ajoute un élève dans l'école si c'est possible.
  */
 void AjouterEleveDansEcole(Ecole_t *ptr_ecole);
 
 
-/* Fonction :
- * ----------
- * Entrée :
- * Sortie :
- *  Usage :
- * ----------
- * *Description*
+/* Fonction : RechercherEleveDansEcole
+ * -----------------------------------
+ * Entrée : ptr_ecole - Pointeur sur l'école dans laquelle on recherche l'élève.
+ * Sortie : Un pointeur sur l'élève si il est trouvé. Renvoie NULL sinon.
+ *  Usage : ptr_eleveRecherche = RechercherEleveDansEcole(ptr_ecole);
+ * -----------------------------------
+ * Recherche un élève dans l'école.
  */
 Eleve* RechercherEleveDansEcole(Ecole_t *ptr_ecole);
 
 
 
-/* Fonction :
- * ----------
- * Entrée :
- * Sortie :
- *  Usage :
- * ----------
- * *Description*
+/* Fonction : SupprimerEleveDansEcole
+ * ----------------------------------
+ * Entrée : ptr_ecole - Pointeur sur l'école dans laquelle on veut supprimer l'élève.
+ * Sortie : Aucune.
+ *  Usage : SupprimerEleveDansEcole(ptr_ecole);
+ * ----------------------------------
+ * Supprime un élève de l'école.
  */
 void SupprimerEleveDansEcole(Ecole_t *ptr_ecole);
 
